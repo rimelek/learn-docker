a57d0d60567e4884266aeafcca2ef0997bddcf8b
Takács Ákos <rimelek@rimelek.hu>
2017-09-11 21:22:52 +0200

    XIP aldomain nélkül

--- system/usr/local/bin/xip.sh
+++ system/usr/local/bin/xip.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-DOMAIN=${1:?First argument is required}
+DOMAIN=${1}
+PREFIX=${DOMAIN}
+
+if [ ! -z "${PREFIX}" ]; then
+    PREFIX=${PREFIX}.
+fi
 
 IP=$(ip route get 8.8.8.8 | awk '{print $NF; exit}')
 
-echo ${DOMAIN}.${IP}.xip.io
+echo ${PREFIX}${IP}.xip.io
